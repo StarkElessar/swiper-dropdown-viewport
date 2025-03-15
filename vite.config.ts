77c1f8a55bc4300cb598f1570c05084e9442ca5a
stark <serj.elessar@gmail.com>
2025-03-15 21:47:16 +0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	base: '/swiper-dropdown-viewport/',
+export default defineConfig(({ command }) => {
+	return {
+		base: command === 'serve' ? '/' : '/swiper-dropdown-viewport/',
+		server: {
+			host: '0.0.0.0',
+			port: 3000
+		}
+	};
 });
